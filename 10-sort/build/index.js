7cc49b510ab1db39fdda368f142c5210386da30c
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-04 17:27:44 +0900

    feat: delete build folder

--- 10-sort/build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const NumbersCollection_1 = require("./NumbersCollection");
-const sorter_1 = require("./sorter");
-const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-const sorter = new sorter_1.Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data);
