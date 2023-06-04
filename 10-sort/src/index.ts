72dbb164ea4991a8b269d6f740941ba0953000df
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-04 17:27:06 +0900

    92. Sorting Arbitrary Collections

--- 10-sort/src/index.ts
+++ 10-sort/src/index.ts
@@ -1,7 +1,12 @@
-import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 import { Sorter } from './sorter';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
