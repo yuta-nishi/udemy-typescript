0338b03971ac0043179a0cf74608bf509821aeec
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-04 18:37:19 +0900

    100. Solving All Issues with Abstract Classes

--- 10-sort/src/index.ts
+++ 10-sort/src/index.ts
@@ -1,14 +1,11 @@
 import { LinkedList } from './LinkedList';
-import { Sorter } from './Sorter';
 
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 
 // const charactersCollection = new CharactersCollection('Xaayb');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
+// charactersCollection.sort();
 // console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -18,7 +15,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
-
+linkedList.sort();
 linkedList.print();
